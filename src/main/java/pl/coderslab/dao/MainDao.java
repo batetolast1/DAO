83a6e23dc1f148aa63e2d28745fe1998351ba18b
Bartosz Robakowski <batetolast1@gmail.com>
2020-06-27 15:36:27 +0200

    created DbUtil, User and UserDao classes, imported BCrypt implementation for Java from https://www.mindrot.org/projects/jBCrypt/, added MainDao class for testing and store all SQL commands used in SQL_commands.txt for convenience

---
+++ src/main/java/pl/coderslab/dao/MainDao.java
@@ -0,0 +1,83 @@
+package pl.coderslab.dao;
+
+import org.mindrot.jbcrypt.BCrypt;
+import pl.coderslab.entity.User;
+
+public class MainDao {
+
+    public static void main(String[] args) {
+        UserDao userDao = new UserDao();
+
+        System.out.println("Creating new users");
+        User user1 = new User();
+        user1.setEmail("user1@gmail.com");
+        user1.setUserName("user1");
+        user1.setPassword("password1");
+        user1 = userDao.create(user1);
+        System.out.println(user1);
+
+        User user2 = new User();
+        user2.setEmail("user2@gmail.com");
+        user2.setUserName("user2");
+        user2.setPassword("password2");
+        user2 = userDao.create(user2);
+        System.out.println(user2);
+
+        User user3 = new User();
+        user3.setEmail("user3@gmail.com");
+        user3.setUserName("user3");
+        user3.setPassword("password3");
+        user3 = userDao.create(user3);
+        System.out.println(user3);
+
+        System.out.println("Creating invalid user:");
+        User user4 = new User();
+        user4 = userDao.create(user4);
+        System.out.println(user4);
+
+        System.out.println("\nReading user");
+        System.out.println(userDao.read(1));
+        System.out.println("Reading non-existing user");
+        System.out.println(userDao.read(4));
+
+        System.out.println("\nChanging user data");
+        System.out.println("User to update:");
+        user1 = userDao.read(1);
+        System.out.println(user1);
+        user1.setEmail("user1@interia.pl");
+        user1.setUserName("USER1");
+        user1.setPassword("new_password1");
+        userDao.update(user1);
+        user1 = userDao.read(1);
+        System.out.println("User after update");
+        System.out.println(user1);
+
+        System.out.println("\nChecking password match");
+        System.out.println(user1.checkPasswordMatch("new_password1"));
+        System.out.println(user1.checkPasswordMatch("wrong_password"));
+
+        System.out.println("\nPrinting all users");
+        User[] allUsers = userDao.findAll();
+        for (User user : allUsers) {
+            System.out.println(user);
+        }
+
+        System.out.println("\nDeleting user");
+        userDao.delete(1);
+        System.out.println(userDao.read(1));
+
+        System.out.println("\nPrinting all users");
+        allUsers = userDao.findAll();
+        for (User user : allUsers) {
+            System.out.println(user);
+        }
+
+        System.out.println("\nDeleting all users");
+        userDao.deleteAll();
+        allUsers = userDao.findAll();
+        System.out.println("Printing all users");
+        for (User user : allUsers) {
+            System.out.println(user);
+        }
+    }
+}
---
+++ src/main/java/pl/coderslab/dao/UserDao.java
@@ -0,0 +1,152 @@
+package pl.coderslab.dao;
+
+import org.mindrot.jbcrypt.BCrypt;
+import pl.coderslab.entity.User;
+
+import java.sql.*;
+import java.util.Arrays;
+
+public class UserDao {
+    private static final String CREATE_USER_QUERY =
+            "INSERT INTO `users`(`email`, `username`, `password`) VALUES (?, ?, ?)";
+    private static final String READ_USER_QUERY = "SELECT * FROM `users` WHERE `id` = ?";
+    private static final String UPDATE_USER_QUERY =
+            "UPDATE `users` SET `email` = ?, `username` = ?, `password` = ? WHERE `id` = ?";
+    private static final String DELETE_USER_QUERY = "DELETE FROM `users` WHERE `id` = ?";
+    private static final String FIND_ALL_USERS_QUERY = "SELECT * FROM `users`";
+    private static final String DELETE_ALL_USERS_QUERY = "DELETE FROM `users`";
+
+    public User create(User user) {
+        try (Connection connection = DbUtil.getConnection();
+             PreparedStatement statement = getCreateStatement(connection, user)) {
+            statement.executeUpdate(); // for UPDATE, INSERT, DELETE
+            ResultSet resultSet = statement.getGeneratedKeys();
+            if (resultSet.next()) {
+                user.setId(resultSet.getInt(1));
+            }
+            return user;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+
+    private PreparedStatement getCreateStatement(Connection connection, User user) throws SQLException {
+        PreparedStatement statement = connection.prepareStatement(CREATE_USER_QUERY, Statement.RETURN_GENERATED_KEYS);
+        statement.setString(1, user.getEmail());
+        statement.setString(2, user.getUserName());
+        statement.setString(3, hashPassword(user.getPassword()));
+        return statement;
+    }
+
+    private String hashPassword(String password) {
+        return BCrypt.hashpw(password, BCrypt.gensalt());
+    }
+
+    public User read(int userId) {
+        try (Connection connection = DbUtil.getConnection();
+             PreparedStatement statement = getReadStatement(connection, userId)) {
+            ResultSet resultSet = statement.executeQuery(); // for SELECT instructions
+            if (resultSet.next()) {
+                return generateUserFrom(resultSet);
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+
+    private PreparedStatement getReadStatement(Connection connection, int userId) throws SQLException {
+        PreparedStatement statement = connection.prepareStatement(READ_USER_QUERY);
+        statement.setInt(1, userId);
+        return statement;
+    }
+
+    // TODO: 27.06.2020 add search for other fields
+
+    private User generateUserFrom(ResultSet resultSet) throws SQLException {
+        User user = new User();
+        user.setId(resultSet.getInt("id"));
+        user.setEmail(resultSet.getString("email"));
+        user.setUserName(resultSet.getString("username"));
+        user.setPassword(resultSet.getString("password"));
+        return user;
+    }
+
+    // TODO: 27.06.2020 add option to return value if update was successful
+    // stmt.getUpdateCount() method returns the number of rows affected.
+
+    public void update(User user) {
+        try (Connection connection = DbUtil.getConnection();
+             PreparedStatement statement = getUpdateStatement(connection, user)) {
+            statement.executeUpdate(); // for UPDATE, INSERT, DELETE
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private PreparedStatement getUpdateStatement(Connection connection, User user) throws SQLException {
+        PreparedStatement statement = connection.prepareStatement(UPDATE_USER_QUERY);
+        statement.setString(1, user.getEmail());
+        statement.setString(2, user.getUserName());
+        statement.setString(3, hashPassword(user.getPassword()));
+        statement.setInt(4, user.getId());
+        return statement;
+    }
+
+    // TODO: 27.06.2020 add update for each field
+
+    public void delete(int userId) {
+        try (Connection connection = DbUtil.getConnection();
+             PreparedStatement statement = getDeleteStatement(connection, userId)) {
+            statement.executeUpdate(); // for UPDATE, INSERT, DELETE
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private PreparedStatement getDeleteStatement(Connection connection, int userId) throws SQLException {
+        PreparedStatement statement = connection.prepareStatement(DELETE_USER_QUERY);
+        statement.setInt(1, userId);
+        return statement;
+    }
+
+    public void deleteAll() {
+        try (Connection connection = DbUtil.getConnection();
+             PreparedStatement statement = getDeleteAllStatement(connection)) {
+            statement.executeUpdate(); // for UPDATE, INSERT, DELETE
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private PreparedStatement getDeleteAllStatement(Connection connection) throws SQLException {
+        return connection.prepareStatement(DELETE_ALL_USERS_QUERY);
+    }
+
+    public User[] findAll() {
+        try (Connection connection = DbUtil.getConnection();
+             PreparedStatement statement = getFindAllStatement(connection)) {
+            ResultSet resultSet = statement.executeQuery(); // for SELECT instructions
+            User[] allUsers = new User[0];
+            while (resultSet.next()) {
+                User user = generateUserFrom(resultSet);
+                allUsers = addToArray(user, allUsers);
+            }
+            return allUsers;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+
+    private PreparedStatement getFindAllStatement(Connection connection) throws SQLException {
+        return connection.prepareStatement(FIND_ALL_USERS_QUERY);
+    }
+
+    private User[] addToArray(User user, User[] users) {
+        User[] tempUsers = Arrays.copyOf(users, users.length + 1);
+        tempUsers[users.length] = user;
+        return tempUsers;
+    }
+}
